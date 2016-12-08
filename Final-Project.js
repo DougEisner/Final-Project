ae1361ec7fdbeb6fdf36b37331c799d1a72c244d
DougEisner <douglaseisner@Douglas-Eisners-MacBook-Pro.local>
2016-12-08 16:39:29 -0500

    Final Submission
    buttons work, but unable to hide text.

--- Final-Project.js
+++ Final-Project.js
@@ -1,7 +1,22 @@
-console.log("test");
 
+var n = 0
 function buyButtonJava () {
-  $('buy-button').css('background-color', 'blue');
+  $('.buy-button').css('background-color', 'yellow');
+  $('.buy-button').text("ADDED TO CART!");
+  n = n + 1
+  $('.count').text(n);
+  function changeBack () {
+    $('.buy-button').css('background-color', 'white');
+    $('.buy-button').text("BUY NOW!");
+  }
+  setTimeout(changeBack, 600);
 }
-
 $('.buy-button').click(buyButtonJava);
+
+
+// ATTEMPT AT HIDING TEXT
+// function productDetails () {
+//   $('.changingText').text(<h4>Exciting Topic</h4>
+//       <h5>Content here</h5>);
+// }
+// $('.prodDetails').click(productDetails);
